c4d9467e5c49c0cbc54addf99cd50e925617a468
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2022-01-23 15:44:46 +0530

    Add Nav Itesm

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.styles';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon,
+NavMenu, NavItem, NavLinks } from './Navbar.styles';
 
 const Navbar = () => {
 
     const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-        setClick(!click);
-    }
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
   return (
       <>
@@ -24,6 +24,24 @@ const Navbar = () => {
                         onClick={handleClick}>
                         { click ? <FaTimes /> : <FaBars /> }
                     </MobileIcon>
+                    {/* Click={click} below is props */}
+                    <NavMenu onClick={handleClick} click={click}> 
+                        <NavItem>
+                            <NavLinks to='/' onClick={closeMobileMenu}>
+                                Home
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/services' onClick={closeMobileMenu}>
+                                Services
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/products' onClick={closeMobileMenu}>
+                                Products
+                            </NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </IconContext.Provider>
